5254402ad98787279af6f97d06b2e860e5823219
Stanley Nguyen <hung.ngn.the@gmail.com>
2017-03-07 16:30:41 +0800

    WIP game queue and countdown

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,23 +2,10 @@ module.exports = (server) => {
   const io = require('socket.io')(server);
   let waitingQueue = [];
   
-  function checkQ() {
-    console.log(waitingQueue.map((w) => w.id));
-    if (waitingQueue.length < 2) return;
-    const player1 = waitingQueue[0];
-    const player2 = waitingQueue[1];
-    waitingQueue = waitingQueue.slice(2);
-    console.log(waitingQueue.map((w) => w.id));
-    const name = player1.id + '#' + player2.id;
-    player1.join(name);
-    player2.join(name);
-    io.to(name).emit('start');
-  }
-  
   io.on('connection', (socket) => {
     socket.on('join', () => {
       waitingQueue.push(socket);
-      checkQ();
+      checkQ(io, waitingQueue);
     });
     
     socket.on('message', () => {
@@ -36,3 +23,35 @@ module.exports = (server) => {
   
   
 };
+
+function checkQ(io, waitingQueue) {
+  console.log(waitingQueue.map((w) => w.id));
+  if (waitingQueue.length < 2) return;
+  const player1 = waitingQueue[0];
+  const player2 = waitingQueue[1];
+  waitingQueue = waitingQueue.slice(2);
+  console.log(waitingQueue.map((w) => w.id));
+  const name = player1.id + '#' + player2.id;
+  player1.join(name);
+  player2.join(name);
+  io.to(name).emit('start');
+  const moleEmitter = emitMole(io, name);
+  countDown(io, name, moleEmitter);
+}
+
+function emitMole(io, roomName) {
+  let mole;
+  return setInterval(() => {
+    mole = randMole();
+    io.to(roomName).emit('mole', mole);
+  }, 500);
+}
+
+function countDown(io, roomName, emitter) {
+  setTimeout(() => {
+    clearInterval(emitter);
+    io.to(roomName).emit('end');
+  }, 10000);
+}
+
+const randMole = () => Math.floor(Math.random() * 9);
--- server/template/script.js
+++ server/template/script.js
@@ -6,31 +6,15 @@ const moles = document.querySelectorAll('.mole');
 const items = document.querySelectorAll('.item');
 const scoreBoard = document.querySelector('#score');
 const manaBar = document.querySelector('.mana');
-let lastMoleIdx;
 let score = 0;
 let mana = 0;
 
-function randTime(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-function randMole(moles) {
-  const idx = Math.floor(Math.random() * moles.length);
-  if (idx === lastMoleIdx) {
-    return randMole(moles);
-  }
-  lastMoleIdx = idx;
-  return moles[idx];
-}
-
-function peep() {
-  const time = randTime(200, 1000);
-  const mole = randMole(moles);
+function peep(moleIdx) {
+  const mole = moles[moleIdx];
   mole.classList.add('up');
   setTimeout(function() {
     mole.classList.remove('up');
-    peep();
-  }, time);
+  }, 500);
 }
 
 function updateManaStatus() {
@@ -45,7 +29,6 @@ function checkItems() {
 }
 
 function hit(e) {
-  socket.emit('message');
   if (!e.isTrusted) return;
   score++;
   if (mana < 100) mana += 10;
@@ -64,5 +47,4 @@ function useItem() {
 moles.forEach(function(m) { m.addEventListener('click', hit); });
 items.forEach(function(i) { i.addEventListener('click', useItem); });
 
-socket.on('start', peep);
-window.addEventListener('beforeunload', console.log.bind(console, new Date()));
+socket.on('mole', peep);
